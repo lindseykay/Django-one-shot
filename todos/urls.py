3648f3671354bcad8a98a4a40a1141ad28a38605
Lindsey <lindseykaycarlson@outlook.com>
2022-09-07 16:52:55 -0700

    Feature 7 Complete-TodoList ListView

---
+++ todos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from todos.views import (
+    TodoListListView,
+)
+
+urlpatterns = [
+    path("", TodoListListView.as_view(), name="todo_list_list"),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
+from todos.models import TodoList, TodoItem
 # Create your views here.
+
+class TodoListListView(ListView):
+    model = TodoList
+    template_name = "todo_lists/list.html"
+#context will be "todolist_list"
