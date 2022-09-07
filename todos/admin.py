65ee5f6a2ed35cb2b9f7b736e8bdd186ad031f48
Lindsey <lindseykaycarlson@outlook.com>
2022-09-07 13:51:58 -0700

    Feature 6 complete-registered TodoItem model in admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TodoList
+from .models import TodoList, TodoItem
 
 # Register your models here.
 
 admin.site.register(TodoList)
+admin.site.register(TodoItem)
