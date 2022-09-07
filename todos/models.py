451954b3f207274e31ff1fc41b652d9ece922156
Lindsey <lindseykaycarlson@outlook.com>
2022-09-07 13:10:42 -0700

    Feature 5 Complete-created TodoItem model

--- todos/models.py
+++ todos/models.py
@@ -4,7 +4,16 @@ from django.db import models
 
 class TodoList(models.Model):
     name = models.CharField(max_length=100)
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class TodoItem(models.Model):
+    task = models.CharField(max_length=100)
+    due_date = models.DateField("%A, %B %e, %Y", null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    list = models.ForeignKey(TodoList, related_name="items", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task
