dfc3925f280104a9ffd9c253b9d83913761a22da
Lindsey <lindseykaycarlson@outlook.com>
2022-09-07 17:54:30 -0700

    Feature 8 complete-detail view/page

--- todos/urls.py
+++ todos/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from todos.views import (
     TodoListListView,
+    TodoListDetailView,
 )
 
 urlpatterns = [
     path("", TodoListListView.as_view(), name="todo_list_list"),
+    path("<int:pk>/", TodoListDetailView.as_view(), name="todo_list_detail"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from todos.models import TodoList, TodoItem
 # Create your views here.
@@ -8,3 +9,9 @@ class TodoListListView(ListView):
     model = TodoList
     template_name = "todo_lists/list.html"
 #context will be "todolist_list"
+
+class TodoListDetailView(DetailView):
+    model = TodoList
+    template_name = "todo_lists/detail.html"
+
+#context will be "todolist"
